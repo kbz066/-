1c27dafbdbc22c00dad1286962669f66d27ce957
kbz <18211571509@163.com>
2019-03-11 12:25:08 +0800

    完成左边导航

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react'
 
+import menuList from '../../config/menuConfig';
+import { Menu } from 'antd';
+import "../NavLeft/index.less"
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+
 
 export default class NavLeft extends Component {
+    constructor() {
+        super();
+        this.state = {
+            menuData: []
+        }
+    }
+
+    componentDidMount() {
+
+        let menuData = this.renderMenu(menuList);
+        this.setState({
+            menuData: menuData
+        })
+    }
+
+    renderMenu = (data) => {
+
+
+        return data.map((item) => {
+            if (item.children) {
+
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+
+
+            }
+            return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+
+        })
+    }
+
+
     render() {
+
+
         return (
             <div>
                 <div className="logo">
-                    <img />
-                    Logo
+                    <img src="../../resource/assets/logo.svg" />
+                    <h1>React MS</h1>
                 </div>
 
+                <Menu mode="vertical" theme="dark">
+                    {
+                        this.state.menuData
+                    }
+                </Menu>
 
 
             </div>
